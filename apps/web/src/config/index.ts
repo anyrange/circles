12ca86985c999f59c6cfd86c2f33217d752edaa8
thaseyor <mikeston@mail.ru>
2023-03-21 16:46:45 +0300

    chore: remove redundant scopes

--- apps/web/src/config/index.ts
+++ apps/web/src/config/index.ts
@@ -1,8 +1,6 @@
 export const SCOPES = [
-  "playlist-read-collaborative",
   "user-read-currently-playing",
   "user-read-recently-played",
-  "playlist-read-private",
   "user-read-private",
   "user-library-read",
   "user-follow-read",
