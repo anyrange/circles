95205d9420472d61f74bafc200ef8b0d3254ab74
Alex Sehl <widesehl@gmail.com>
2026-03-22 16:03:15 +0500

    test: add unit tests

--- apps/frontend/src/test/setup.ts
+++ apps/frontend/src/test/setup.ts
@@ -1,7 +1,9 @@
+import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "vite-plus/test";
+import { afterEach, expect } from "vite-plus/test";
 
 expect.extend(matchers);
+afterEach(cleanup);
 
 // @ts-expect-error
 global.IS_REACT_ACT_ENVIRONMENT = true;
